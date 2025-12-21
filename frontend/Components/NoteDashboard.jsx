a3ab86629983aec2d1191a5e11d6a10b665a6ec2
Palden D. Titung <paldendorjetitung@gmail.com>
2025-12-21 23:03:52 +0545

    added the search and filtering

--- frontend/Components/NoteDashboard.jsx
+++ frontend/Components/NoteDashboard.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NoteCard from "./NoteCard";
 import { getNotes } from "../Services/noteService";
 const NoteDashboard = ({ data, setData }) => {
+  const [searchItem, setSearchITem] = useState("");
+  const [subjectFilter, setSubjectFilter] = useState("");
+  const [semester, setSemester] = useState("");
   useEffect(() => {
     const fetchNotes = async () => {
       const notes = await getNotes();
@@ -13,59 +16,16 @@ const NoteDashboard = ({ data, setData }) => {
     console.log("Updated notes:", data);
   }, [data]);
 
-  const notesData = [
-    {
-      id: "1",
-      title: "Binary Trees",
-      subject: "DSA",
-      semester: 3,
-      description: "Lecture notes on Binary Trees with examples and exercises.",
-      fileType: "pdf",
-      isFavorite: false,
-      filePath: "uploads/dsa/binary_trees.pdf",
-    },
-    {
-      id: "2",
-      title: "Sorting Algorithms",
-      subject: "DSA",
-      semester: 3,
-      description:
-        "Notes covering Bubble, Merge, Quick, and Heap Sort algorithms.",
-      fileType: "pdf",
-      isFavorite: true,
-      filePath: "uploads/dsa/sorting_algorithms.pdf",
-    },
-    {
-      id: "3",
-      title: "Normalization",
-      subject: "DBMS",
-      semester: 2,
-      description: "Notes on 1NF, 2NF, 3NF with examples.",
-      fileType: "pdf",
-      isFavorite: false,
-      filePath: "uploads/dbms/normalization.pdf",
-    },
-    {
-      id: "4",
-      title: "ER Diagrams",
-      subject: "DBMS",
-      semester: 2,
-      description: "Entity-Relationship diagrams for database design.",
-      fileType: "image",
-      isFavorite: true,
-      filePath: "uploads/dbms/er_diagram.png",
-    },
-    {
-      id: "5",
-      title: "Derivatives & Integrals",
-      subject: "Math",
-      semester: 1,
-      description: "Basic calculus notes covering derivatives and integrals.",
-      fileType: "pdf",
-      isFavorite: false,
-      filePath: "uploads/math/calculus.pdf",
-    },
-  ];
+  const filterData = data.filter((data) => {
+    const searchQuery =
+      data.title.toLowerCase().includes(searchItem.toLowerCase()) ||
+      data.description.toLowerCase().includes(searchItem.toLowerCase());
+
+    const matchSubject = subjectFilter ? data.subject === subjectFilter : true;
+
+    const matchSemester = semester ? data.semester === semester : true;
+    return searchQuery && matchSubject && matchSemester;
+  });
 
   return (
     <section className="flex flex-col gap-7 ">
@@ -76,13 +36,19 @@ const NoteDashboard = ({ data, setData }) => {
           <input
             type="search"
             placeholder="Search notes..."
+            value={searchItem}
+            onChange={(e) => setSearchITem(e.target.value)}
             className="p-2 rounded w-full outline-0 border-0 ring-2 ring-gray-400 focus:ring-mainColor transition-all duration-300"
           />
         </div>
 
         {/* Subject Filter */}
         <div className="flex-1 ">
-          <select className="py-2.5 px-2 rounded w-full outline-0 border-0 ring-2 ring-gray-400 focus:ring-mainColor transition-all duration-300">
+          <select
+            value={subjectFilter}
+            onChange={(e) => setSubjectFilter(e.target.value)}
+            className="py-2.5 px-2 rounded w-full outline-0 border-0 ring-2 ring-gray-400 focus:ring-mainColor transition-all duration-300"
+          >
             <option value="">All Subjects</option>
             <option value="DSA">DSA</option>
             <option value="DBMS">DBMS</option>
@@ -93,7 +59,11 @@ const NoteDashboard = ({ data, setData }) => {
 
         {/* Semester Filter */}
         <div className="flex-1 ">
-          <select className="py-2.5 px-2 rounded w-full outline-0 border-0 ring-2 ring-gray-400 focus:ring-mainColor transition-all duration-300">
+          <select
+            value={semester}
+            onChange={(e) => setSemester(e.target.value)}
+            className="py-2.5 px-2 rounded w-full outline-0 border-0 ring-2 ring-gray-400 focus:ring-mainColor transition-all duration-300"
+          >
             <option value="">All Semesters</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -106,7 +76,7 @@ const NoteDashboard = ({ data, setData }) => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-        {data.map((note) => (
+        {filterData.map((note) => (
           <NoteCard key={note.id} note={note} />
         ))}
       </div>
